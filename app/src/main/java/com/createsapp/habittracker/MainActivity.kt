818964c155c59e68f83d89cefdc4a7495711721c
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-09-17 17:02:13 +0530

    Calculating time between two dates

--- app/src/main/java/com/createsapp/habittracker/MainActivity.kt
+++ app/src/main/java/com/createsapp/habittracker/MainActivity.kt
@@ -2,10 +2,21 @@ package com.createsapp.habittracker
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupActionBarWithNavController(findNavController(R.id.navHostFragment))
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.navHostFragment)
+        return super.onSupportNavigateUp()
+    }
+
 }
